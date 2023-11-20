2099848e558c95bc3efc7cf337fc53ed4c240fee
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-11-20 14:07:24 +0100

    Tillat kall fra data.intern.dev.nav.no

--- src/main/kotlin/no/nav/helse/Application.kt
+++ src/main/kotlin/no/nav/helse/Application.kt
@@ -5,12 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import io.ktor.http.HttpMethod.Companion.Get
-import io.ktor.http.HttpMethod.Companion.Post
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationCallPipeline
 import io.ktor.server.application.call
+import io.ktor.server.application.install
+import io.ktor.server.plugins.cors.routing.CORS
 import io.ktor.server.request.httpMethod
 import io.ktor.server.request.uri
 import io.ktor.server.response.respond
@@ -28,6 +30,13 @@ private const val BASE_PATH = "/syk/flexjar"
 fun Application.main() {
     val log = LoggerFactory.getLogger("no.nav.helse.Application.main")
 
+    install(CORS) {
+        allowHost("data.intern.dev.nav.no", schemes = listOf("https"))
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowHeader(HttpHeaders.ContentType)
+    }
+
     intercept(ApplicationCallPipeline.Call) {
         val httpMethod = call.request.httpMethod
         val requestUri = call.request.uri.replace(BASE_PATH, "")
@@ -41,11 +50,11 @@ fun Application.main() {
             else -> {
                 log.info("Intercepted ${httpMethod.value} call to \"${requestUri}\".")
                 when (httpMethod) {
-                    Post -> {
+                    HttpMethod.Post -> {
                         call.respond(HttpStatusCode.Accepted)
                     }
 
-                    Get -> {
+                    HttpMethod.Get -> {
                         call.respond(HttpStatusCode.OK)
                     }
 
