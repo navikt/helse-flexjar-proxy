7093951b4470cc359af7ad02a9288c63b18390c8
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-11-21 12:47:11 +0100

    Fjern ubrukt import

--- src/test/kotlin/no/nav/helse/ApplicationIntegrationTest.kt
+++ src/test/kotlin/no/nav/helse/ApplicationIntegrationTest.kt
@@ -4,7 +4,6 @@ import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.put
 import io.ktor.client.statement.HttpResponse
-import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.testApplication
 import org.junit.jupiter.api.Assertions.assertEquals
